bd53a7c4b45fc9041e2416e86f2b8764b2bc05d3
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2016-11-22 23:05:34 +0300

    Completed SecondPartTasks

--- Task4/src/main/java/ru/spbau/bogomolov/SecondPartTasks.java
+++ Task4/src/main/java/ru/spbau/bogomolov/SecondPartTasks.java
@@ -1,7 +1,13 @@
 package ru.spbau.bogomolov;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
@@ -9,25 +15,77 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths
+                .stream()
+                .flatMap(
+                        path -> {
+                            Stream<String> lines = null;
+                            try {
+                                lines = Files.lines(Paths.get(path));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            return lines;
+                        }
+                )
+                .filter(line -> line.contains(sequence))
+                .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
     // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        class Pair {
+            private double x, y;
+            private Pair (double x, double y) {
+                this.x = x;
+                this.y = y;
+            }
+            private boolean check() {
+                return (x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5) <= 0.25;
+            }
+        }
+        final int POINTS = 10000000;
+        Random random = new Random();
+        return Stream.generate(() -> new Pair(random.nextDouble(), random.nextDouble()))
+                .limit(POINTS)
+                .filter(Pair::check)
+                .count() * 1. / POINTS;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions
+                .entrySet()
+                .stream()
+                .max(
+                    Comparator.comparingInt(
+                            comps -> comps
+                                    .getValue()
+                                    .stream()
+                                    .mapToInt(String::length)
+                                    .sum()
+                    )
+                )
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders
+                .stream()
+                .flatMap(
+                        order -> order.entrySet().stream()
+                )
+                .collect(
+                        Collectors.groupingBy(
+                                Map.Entry::getKey,
+                                Collectors.summingInt(Map.Entry::getValue)
+                        )
+                );
     }
 }
--- Task4/src/test/java/ru/spbau/bogomolov/SecondPartTasksTest.java
+++ Task4/src/test/java/ru/spbau/bogomolov/SecondPartTasksTest.java
@@ -1,28 +1,107 @@
 package ru.spbau.bogomolov;
 
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.Array;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 public class SecondPartTasksTest {
 
     @Test
-    public void testFindQuotes() {
-        fail();
+    public void testFindQuotes() throws IOException {
+        TemporaryFolder folder = new TemporaryFolder();
+        folder.create();
+        File f1 = folder.newFile("f1");
+        File f2 = folder.newFile("f2");
+        File f3 = folder.newFile("f3");
+
+        PrintStream printStream = new PrintStream(f1);
+        String l1 = "It's a test string";
+        printStream.println(l1);
+        String l2 = "testing is important";
+        printStream.println(l2);
+        String l3 = "t e s t s e v e r y w h e r e";
+        printStream.println(l3);
+
+        printStream = new PrintStream(f2);
+        String l4 = ".....tests......";
+        printStream.println(l4);
+        String l5 = "TestTESTtEsTtesT";
+        printStream.println(l5);
+        String l6 = "test";
+        printStream.println(l6);
+
+        printStream = new PrintStream(f3);
+        String l7 = "";
+        printStream.println(l7);
+        String l8 = "1234567890"; printStream.println(l8);
+        String l9 = "l;aslkdf;asjdf;alskjdftestaksdjf;alskdjf";
+        printStream.println(l9);
+
+        List<String> correctResult = Arrays.asList(l1, l2, l4, l6, l9);
+        Collections.sort(correctResult);
+
+        List<String> result = SecondPartTasks.findQuotes(
+                Arrays.asList(f1.getPath(), f2.getPath(), f3.getPath()), "test");
+        Collections.sort(result);
+
+        assertEquals(correctResult, result);
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        final double PRECISION = 1e-3;
+        for (int i = 0; i < 5; i++) {
+            assertTrue(Math.abs(SecondPartTasks.piDividedBy4() - Math.PI / 4) < PRECISION);
+        }
+    }
+
+    @Test
+    public void testFindPrinterEmptyMap() {
+        assertNull(SecondPartTasks.findPrinter(Collections.emptyMap()));
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        Map<String, List<String>> compositions = new HashMap<>();
+        compositions.put("1", Arrays.asList("", "", ""));
+        compositions.put("2", Arrays.asList("a", "b", "c", "d", "e"));
+        compositions.put("3", Arrays.asList("abc", "def", "ghi"));
+        compositions.put("4", Arrays.asList("abcd", "efgh"));
+
+        assertEquals("3", SecondPartTasks.findPrinter(compositions));
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        Map<String, Integer> ord1 = new HashMap<>();
+        ord1.put("1", 100);
+        ord1.put("2", 200);
+        ord1.put("3", 300);
+
+        Map<String, Integer> ord2 = new HashMap<>();
+        ord2.put("2", 100);
+        ord2.put("3", 200);
+        ord2.put("4", 300);
+
+        Map<String, Integer> ord3 = new HashMap<>();
+        ord3.put("1", 100);
+        ord3.put("3", 200);
+        ord3.put("5", 300);
+
+        Map<String, Integer> expectedMap = new HashMap<>();
+        expectedMap.put("1", 200);
+        expectedMap.put("2", 300);
+        expectedMap.put("3", 700);
+        expectedMap.put("4", 300);
+        expectedMap.put("5", 300);
+
+        assertEquals(expectedMap, SecondPartTasks.calculateGlobalOrder(Arrays.asList(ord1, ord2, ord3)));
     }
 }
